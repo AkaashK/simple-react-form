4cf77acdc424baa07e61a03ded098586b6afa23b
Akaash Kannan <akaash324it@gmail.com>
2020-04-29 15:14:14 +0530

    Error Message added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+
+import { useForm, ErrorMessage } from "react-hook-form";
+
+const pricePerBox = 20;
+
+const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
 export default function App() {
   const [user, setUser] = useState({
@@ -13,62 +18,87 @@ export default function App() {
   const { register, errors, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
-    const receivedUser = {
-      name: data.name,
-      address: data.address,
-      stock: data.stock,
-      email: data.email,
-      received: true,
-    };
-
+    const receivedUser = { ...data, received: true };
     const assignUser = Object.assign(user, receivedUser);
     setUser(assignUser);
-    console.log(user);
-  };
-
-  const calculatePrice = () => {
-    const pricePerBox = 20;
-    return pricePerBox * user.stock;
   };
 
   return (
     <div>
       <h2>Cool Drink Order Form</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        Name: <input name="name" ref={register({ required: true })} /> <br />
-        {errors.name && "Your input is required"} <br />
+        Name:
+        <input name="name" ref={register({ required: "This is required" })} />
+        <br />
+        <ErrorMessage errors={errors} name="name">
+          {({ message }) => (
+            <p data-testid="nameError" className="form-error">
+              {message}
+            </p>
+          )}
+        </ErrorMessage>
+        <br />
         <label>Address:</label>
         <textarea
           type="TextArea"
           name="address"
-          ref={register({ required: true, maxLength: 100 })}
+          ref={register({
+            required: {
+              value: true,
+              message: "This is required",
+            },
+            maxLength: {
+              value: 100,
+              message: "input exceeds maximum limit",
+            },
+          })}
         />
         <br />
-        {errors.address?.type === "required" && "Your input is required"}
-        {errors.address?.type === "maxLength" && "Your input exceed maxLength"}
+        <ErrorMessage errors={errors} name="address">
+          {({ message }) => (
+            <p data-testid="addressError" className="form-error">
+              {message}
+            </p>
+          )}
+        </ErrorMessage>
         <br />
         Boxes needed:
         <input
-          placeholder="min value should be 50"
+          placeholder="minimum value should be 50"
           type="number"
           name="stock"
-          ref={register({ min: 50 })}
+          ref={register({
+            min: {
+              value: 50,
+              message: "value should be minimum 50",
+            },
+          })}
         />
         <br />
-        {errors.stock && "Your input required to be more than 50"}
+        <ErrorMessage errors={errors} name="stock">
+          {({ message }) => (
+            <p data-testid="stockError" className="form-error">
+              {message}
+            </p>
+          )}
+        </ErrorMessage>
         <br />
         email:
         <input
           name="email"
           ref={register({
             required: "This is required",
-            pattern: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            pattern: emailPattern,
           })}
         />
         <br />
-        {errors.email?.message}
-        {errors.email?.type === "pattern" &&
-          "Your email does not match the pattern"}
+        <ErrorMessage errors={errors} name="email">
+          {({ message }) => (
+            <p data-testid="emailError" className="form-error">
+              {message}
+            </p>
+          )}
+        </ErrorMessage>
         <br />
         <input type="submit" />
       </form>
@@ -77,7 +107,8 @@ export default function App() {
       )}
       {user.received && (
         <p>
-          The total price of ordered boxes is {calculatePrice()}(in dollars)
+          The total price of ordered boxes is {pricePerBox * user.stock}(in
+          dollars)
         </p>
       )}
     </div>
