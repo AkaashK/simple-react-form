7a41bc84c50c0574b5c565c12286756e9d22a7ac
Akaash Kannan <akaash324it@gmail.com>
2020-04-30 17:41:14 +0530

    reset values added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-
 import { useForm, ErrorMessage } from "react-hook-form";
 
 import "./App.css";
 
 const pricePerBox = 20;
-
 const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-export default function App() {
-  const [user, setUser] = useState({});
+let editData = {};
+
+const App = () => {
+  const [user, setUser] = useState([]);
   const [received, setReceived] = useState(false);
+  const [enableEdit, setEnableEdit] = useState();
 
   const { register, errors, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
-    setUser({ ...data, ...user });
+    const savedData = [...user, { ...data }];
+    setUser(savedData);
     setReceived(true);
+
+    editData = savedData[savedData.length - 1];
   };
 
   return (
@@ -24,7 +28,11 @@ export default function App() {
       <h2>Cool Drink Order Form</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Name: </label>
-        <input name="name" ref={register({ required: "Name is required" })} />
+        <input
+          name="name"
+          defaultValue={enableEdit && editData.name}
+          ref={register({ required: "Name is required" })}
+        />
         <br />
         <ErrorMessage errors={errors} name="name">
           {({ message }) => (
@@ -38,6 +46,7 @@ export default function App() {
         <input
           type="text"
           name="address"
+          defaultValue={enableEdit && editData.address}
           ref={register({
             required: {
               value: true,
@@ -63,6 +72,7 @@ export default function App() {
           placeholder="please enter atleast 50"
           type="number"
           name="stock"
+          defaultValue={enableEdit && editData.stock}
           ref={register({
             min: {
               value: 50,
@@ -83,6 +93,7 @@ export default function App() {
         <label htmlFor="email">Email: </label>
         <input
           name="email"
+          defaultValue={enableEdit && editData.email}
           ref={register({
             required: {
               value: true,
@@ -110,19 +121,33 @@ export default function App() {
           type="reset"
           onClick={() => {
             setReceived(false);
-            setUser({});
+            setEnableEdit("");
           }}
         />
+        <button
+          type="button"
+          onClick={() => {
+            setEnableEdit(true);
+            user.pop();
+          }}
+        >
+          Edit
+        </button>
       </form>
       {received && (
         <>
-          <p>order received, stock will be sent to {user.address}</p>
           <p>
-            The total price of ordered boxes is {pricePerBox * user.stock}(in
-            dollars)
+            order received, stock will be sent to
+            {user[user.length - 1].address}
+          </p>
+          <p>
+            The total price of ordered boxes is
+            {pricePerBox * user[user.length - 1].stock}(in dollars)
           </p>
         </>
       )}
     </div>
   );
-}
+};
+
+export default App;
